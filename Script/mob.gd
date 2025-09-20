f4d8920df4addfa55295a2052336c82aa6c49fec
9Nut <keattisak.n@kkumail.com>
2025-09-20 22:31:42 +0700

    finish

---
+++ Script/mob.gd
@@ -0,0 +1,23 @@
+extends CharacterBody3D
+@export var min_speed = 30
+@export var max_speed = 35
+signal squashed
+
+func _physics_process(_delta):
+	$"Node3D/Root Scene/AnimationPlayer".play("CharacterArmature|Run")
+	move_and_slide()
+	
+func initialize(start_position, player_position):
+	look_at_from_position(start_position, player_position, Vector3.UP)
+	rotate_y(randf_range(-PI / 4, PI / 4))
+	var random_speed = randi_range(min_speed, max_speed)
+	velocity = Vector3.FORWARD * random_speed
+	velocity = velocity.rotated(Vector3.UP, rotation.y)
+
+
+func squash():
+	squashed.emit()
+	queue_free()
+
+func _on_visible_on_screen_notifier_3d_screen_exited():
+	queue_free()
